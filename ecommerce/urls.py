227d4892a422c7e9b9c2db9dabb287d46f42bca8
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-07-16 19:43:26 +0430

    home page created.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, header_view, footer_view
 
 app_name = 'ecommerce'
 
 urlpatterns = [
-    path('', home_page_view, name='home_page')
+    path('', home_page_view, name='home_page'),
+    path('header/', header_view, name='header'),
+    path('footer/', footer_view, name='footer'),
+
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home_page_view(request):
     return render(request, 'home_page.html')
+
+
+# <--render_partial_views-->
+def header_view(request):
+    context = {}
+    return render(request, 'shared/Header.html', context)
+
+
+def footer_view(request):
+    context = {}
+    return render(request, 'shared/Footer.html', context)
+# <--/render_partial_views-->
