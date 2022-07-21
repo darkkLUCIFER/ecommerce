ac3ae86ee1a277f792c2c4941f7b48008d91ae86
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-07-21 09:50:16 +0430

    the search section created.

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProudctListView, product_detail_view
+from .views import ProudctListView, product_detail_view, SearchProductView
 
 app_name = 'product'
 
 urlpatterns = [
     path('', ProudctListView.as_view(), name='product_list'),
     path('<int:pk>/', product_detail_view, name='product_detail'),
+    path('search/', SearchProductView.as_view(), name='product_search'),
 
 ]
--- product/views.py
+++ product/views.py
@@ -18,3 +18,17 @@ def product_detail_view(request, pk):
     except Product.DoesNotExist:
         return render(request, 'partials/404.html')
     return render(request, 'product/product_detail.html', {'product': product})
+
+
+class SearchProductView(ListView):
+    model = Product
+    template_name = 'product/products_list.html'
+    context_object_name = 'products'
+    paginate_by = 6
+
+    def get_queryset(self):
+        request = self.request
+        query = request.GET.get('q')
+        if query:
+            return Product.objects.filter(active=True, title__icontains=query)
+        return Product.objects.filter(active=True)
