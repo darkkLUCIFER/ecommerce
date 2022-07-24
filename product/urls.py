fbbe242cfd8c086d161cbb7042d24cfec29202de
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-07-24 16:00:56 +0430

    make products category list in pages and some filters.

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import ProudctListView, product_detail_view, SearchProductView
+from .views import ProudctListView, product_detail_view, SearchProductView, ProductListByCategory, product_categories, \
+    Error404View
 
 app_name = 'product'
 
 urlpatterns = [
     path('', ProudctListView.as_view(), name='product_list'),
-    path('<int:pk>/', product_detail_view, name='product_detail'),
+    path('<str:category_name>/', ProductListByCategory.as_view(), name='product_list_by_category'),
+    path('detail/<int:pk>/', product_detail_view, name='product_detail'),
     path('search/', SearchProductView.as_view(), name='product_search'),
+    path('product_category/', product_categories, name='product_category'),
+    path('error/404/', Error404View.as_view(), name='error_404'),
+
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Product
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
+from product_category.models import ProductCategory
 
 
 class ProudctListView(ListView):
@@ -10,6 +11,20 @@ class ProudctListView(ListView):
     queryset = Product.objects.filter(active=True)
     context_object_name = 'products'
     paginate_by = 1
+
+
+class ProductListByCategory(ListView):
+    model = Product
+    template_name = 'product/products_list.html'
+    context_object_name = 'products'
+    paginate_by = 6
+
+    def get_queryset(self):
+        category_name = self.kwargs['category_name']
+        category = ProductCategory.objects.filter(title__iexact=category_name).first()
+        if category is None:
+            raise Http404('صفحه مورد نظر یافت نشد.')
+        return Product.objects.filter(active=True).filter(categories__title__iexact=category)
 
 
 def product_detail_view(request, pk):
@@ -32,3 +47,12 @@ class SearchProductView(ListView):
         if query:
             return Product.objects.search(query)
         return Product.objects.filter(active=True)
+
+
+def product_categories(request):
+    categories = ProductCategory.objects.all()
+    return render(request, 'product/product_category.html', {'categories': categories})
+
+
+class Error404View(TemplateView):
+    template_name = 'partials/404.html'
