279c33a379b576c28e388681ba90254cd666c935
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-08-03 19:45:54 +0430

    zarin_pal gateway added to eshop_order app

--- eshop_order/urls.py
+++ eshop_order/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from eshop_order.views import add_user_order, user_open_order
+from eshop_order.views import add_user_order, user_open_order, send_request, verify
 
 app_name = 'eshop_order'
 
 urlpatterns = [
     path('add_user_order/', add_user_order, name='add_user_order'),
     path('user_open_order/', user_open_order, name='user_open_order'),
+    path('request/', send_request, name='request'),
+    path('verify/', verify, name='verify'),
 
 ]
